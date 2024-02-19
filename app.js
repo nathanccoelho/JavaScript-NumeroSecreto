e3e248cbd37d7509b5009bb3a469cfac06adaa4e
nathanccoelho <nathancoelho.job@gmail.com>
2024-02-19 20:32:09 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Boas vindas ao jogo do número secreto!");
+
+let numeroSecreto = 29;
+
+let chute = prompt("Escolha um número entre 1 e 30");
+
+if(numeroSecreto == chute){
+    console.log("Isso ai! Você descorbriu o número secreto")
+}
